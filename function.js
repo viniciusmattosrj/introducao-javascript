630c8af36969bc2543598f37e81b5ffa24cc2a6e
Vinicius Mattos <vinimattos.rj@gmail.com>
2017-09-10 18:04:29 -0300

    Clousers

--- function.js
+++ function.js
@@ -1,31 +1,10 @@
-/* Funções Construtoras vs. Funções Fábrica */
-//Funções Fábrica:
-
-var criarPessoa = function (nome, idade){
-  return{
-    nome: nome,
-    idade: idade
+/* Clousers */
+var helloWorld = function(){
+  var message = "Hello World";
+  return function (){
+    return message;
   };
 };
 
-console.log(criarPessoa("Pedro", 20));
-console.log(criarPessoa("Maria", 30));
-
-
-//Funções Construtoras:
-var Pessoa = function (nome, idade){
-  this.nome = nome;
-  this.idade = idade;
-};
-
-console.log(new Pessoa("Pedro", 20));
-console.log(new Pessoa("Maria", 30));
-
-//Exemplo Construtora com Apply para ilustrar
-var pedro = {};
-Pessoa.call(pedro, "Pedro", 20);
-console.log(pedro);
-
-var maria = {}
-Pessoa.call(maria, "Maria", 30);
-console.log(maria);
+var fnHelloWorld = helloWorld();
+console.log(fnHelloWorld());
