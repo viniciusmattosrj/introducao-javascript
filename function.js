0c50006753691953c8ac2a0fb7190d4bd945bf83
Vinicius Mattos <vinimattos.rj@gmail.com>
2017-09-10 17:42:37 -0300

    Funções Construtoras

--- function.js
+++ function.js
@@ -10,3 +10,12 @@ var criarPessoa = function (nome, idade){
 
 console.log(criarPessoa("Pedro", 20));
 console.log(criarPessoa("Maria", 30));
+
+
+//Funções Construtoras:
+var Pessoa = function (nome, idade){
+  this.nome = nome;
+  this.idade = idade;
+};
+
+console.log(new Pessoa("Pedro", 20));
