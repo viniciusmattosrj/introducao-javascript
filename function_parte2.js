16f94a125bae4f77395a1cb3d8f558086018259e
Vinicius Mattos <vinimattos.rj@gmail.com>
2017-09-13 23:08:51 -0300

    Continuando Função

--- function_parte2.js
+++ function_parte2.js
@@ -5,3 +5,14 @@ var add = function(){
 console.log(add());
 console.log(add());
 console.log(add());
+
+var itens = [];
+var add = function (item){
+	itens.push(item);
+	return itens;
+};
+console.log(add('A'));
+console.log(add('B'));
+console.log(add('C'));
+
+console.log(add());
