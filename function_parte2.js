12f4303cce219a9c85b246742fa7a04323dfe913
Vinicius Mattos <vinimattos.rj@gmail.com>
2017-09-13 23:24:44 -0300

    Factory Function

--- function_parte2.js
+++ function_parte2.js
@@ -1,20 +1,15 @@
-var counter = {
-	value: 0,
-	add: function(){
-		return ++this.value;
-	}
+/* FACTORY FUNCTION */
+/* Função criando contador */
+var createCounter = function (){
+	var value = 0;
+	return{
+		add: function (){
+			return ++value;
+		}
+	};
 };
+var counter = createCounter();
+console.log(counter.value);
 console.log(counter.add());
 console.log(counter.add());
 console.log(counter.add());
-
-var itens = {
-	value: [],
-	add: function (item){
-		this.value.push(item);
-		return this.value;
-	}	
-};
-console.log(itens.add('A'));
-console.log(itens.add('B'));
-console.log(itens.add('C'));
