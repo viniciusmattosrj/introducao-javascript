4c0b8f917064cfacaba219130ea0fdeab4f04d36
Vinicius Mattos <vinimattos.rj@gmail.com>
2017-09-16 19:59:11 -0300

    Flexibilizando os Quantificadores de Caracteres

---
+++ exemplo6.js
@@ -0,0 +1,8 @@
+/* Expressão Regular - 6º Exemplo */
+/* Quantificadores de Caracteres */
+var regExp = /^\([0-9]{2}\) [0-9]{4,5}-[0-9]{4}$/; // Inclusão do escape, validando inicio e final
+var telefone1 = "(80) 9876-1234";
+console.log(regExp.test(telefone1));
+
+var telefone2 = "(80) 99876-1234";
+console.log(regExp.test(telefone2));
