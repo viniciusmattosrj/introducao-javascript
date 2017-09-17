27ae6738e7e08c60e05ba5ea11560b3a40ada746
Vinicius Mattos <vinimattos.rj@gmail.com>
2017-09-16 21:20:43 -0300

    Exemplo 6

--- exemplo6.js
+++ exemplo6.js
@@ -1,5 +1,5 @@
 /* Expressão Regular - 6º Exemplo */
-/* Quantificadores de Caracteres */
+/* Flexibilizando os Quantificadores de Caracteres */
 var regExp = /^\([0-9]{2}\) [0-9]{4,5}-[0-9]{4}$/; // Inclusão do escape, validando inicio e final
 var telefone1 = "(80) 9876-1234";
 console.log(regExp.test(telefone1));
