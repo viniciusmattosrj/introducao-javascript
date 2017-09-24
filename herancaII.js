425c37ae1f223a14ddcafc38b5a1dedb63e5b7bd
Vinicius Mattos <vinimattos.rj@gmail.com>
2017-09-24 20:25:08 -0300

    Exemplo da função new

--- herancaII.js
+++ herancaII.js
@@ -5,13 +5,19 @@ var Homem = function (nome, idade){
 
 Homem.prototype.sexo = "masculino";
 
-var joao = new Homem("João",20);
+var joao = new Homem("João", 20);
 console.log(joao);
 console.log(joao.sexo);
 
-var pedro = {};
-/* O new faz o apontamento para essa linha */
-pedro.__proto__ = Homem.prototype;
-Homem.apply(pedro, ["Pedro", 18]);
+/* Exemplo da função new */
+var _new = function (f){
+  var obj = {};
+  obj.__proto__ = f.prototype;
+  f.apply(obj, Array.prototype.slice.call(arguments, 1));
+  return obj;
+};
+
+var pedro = _new(Homem, "Pedro", 18);
+
 console.log(pedro);
 console.log(pedro.sexo);
