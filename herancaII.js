002e3234471c21ae87bc615ca43cca3c568e9731
Vinicius Mattos <vinimattos.rj@gmail.com>
2017-09-24 18:01:00 -0300

    Herança com apply()

--- herancaII.js
+++ herancaII.js
@@ -8,6 +8,13 @@ var joao = new Homem("João",20);
 console.log(joao);
 
 /* Exemplo com call() */
+/*
 var pedro = {};
 Homem.call(pedro, "Pedro", 18);
+console.log(pedro);
+*/
+
+/* Exemplo com apply() */
+var pedro = {};
+Homem.apply(pedro, ["Pedro", 18]);
 console.log(pedro);
